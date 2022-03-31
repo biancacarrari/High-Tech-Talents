ed88d5749b8b3bae0d53f3293b5e8bb6a5e45e7a
biancacarrari <102032612+biancacarrari@users.noreply.github.com>
2022-03-31 11:26:53 -0300

    Delete 5.py

--- 5.py
+++
@@ -1,3 +0,0 @@
-#5 - Faça um Programa que converta metros para centímetros.
-metros = int(input("Insira a quantidade em metros para conversão: "))
-print ("A conversão para centímetros é: ",metros*100)
