a5a5790b304b0cc813d8d74ba72b6635689f36b1
biancacarrari <102032612+biancacarrari@users.noreply.github.com>
2022-03-31 11:26:38 -0300

    Delete 8.py

--- 8.py
+++
@@ -1,4 +0,0 @@
-# 8 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
-hora = int(input("Insira o quanto vocÊganha por hora: "))
-hora_no_mes = int(input("Insira o número de horas trabalhadas no mês: "))
-print ("O total do seu salário no referido mês é: ",hora*hora_no_mes)
