84547649b2d670c4e2c917bae2ea6dc59a92dfc2
biancacarrari <102032612+biancacarrari@users.noreply.github.com>
2022-03-31 11:25:04 -0300

    Add files via upload

---
+++ Lista_1_21_03/4_4.py
@@ -0,0 +1,14 @@
+# Faça um programa que monte uma lista de compras na feira. Nesse programa deverá conter:
+# -Uma tela para mostrar a lista de compras
+# -Outra tela para cadastrar um item dessa lista
+# -Outra tela para excluir um item (Deverá primeiro mostrar a lista e depois excluir o item conforme posição)
+# -Login ao começar
+# -Opção Sair do programa
+# Dicas:
+# Use Lista
+# Não necessita de usar funções, mas pode usar se achar necessário
+# Não necessita criar classes, mas pode usar se achar necessário
+# Use como base, os exercicios anteriores para montar o programa (Tabuada e o do Troubleshooting feito em sala de aula)
+# Use funções de listas
+# Válido somente para quem faltou no kahoot da semana passada, mas se os outros quiserem fazer para treinar, fiquem a vontade
+# Entrega para próxima semana (04/04)
---
+++ Lista_1_21_03/6.py
@@ -0,0 +1,4 @@
+#6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+raio = int(input("Insira o raio de um círculo: "))
+circunferencia = 2*3.141*raio
+print ("A áerea do círculo é: ",circunferencia)
