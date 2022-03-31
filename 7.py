148147826884cc3d2536c98b3d148f60a3791588
biancacarrari <102032612+biancacarrari@users.noreply.github.com>
2022-03-31 11:26:42 -0300

    Delete 7.py

--- 7.py
+++
@@ -1,5 +0,0 @@
-#7 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-lado = int(input("Insira o tamanho do lado do quadrado: "))
-area = lado**2
-print ("A áerea do quadrado é: ",area)
-print ("O dobro da áerea do quadrado é: ",area*2)
