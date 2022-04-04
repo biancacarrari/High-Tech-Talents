64f53f3e077c830b48c8d3facb334480a90e3b5a
biancacarrari <102032612+biancacarrari@users.noreply.github.com>
2022-04-04 16:03:04 -0300

    Add files via upload

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -1,1 +1,35 @@
+usuario = input("Entre com o seu nome: ")
+print(f"Seja Bem-vindo {usuario} !\n")
 
+while True:
+    print("Selecione uma opção:")
+    print("1 - Adição")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - Sair")
+    opcao = int(input(""))
+    if opcao in [1,2,3,4,5]:
+        num1 = ""
+        num2 = ""
+        while num1 == "":
+            num1 = int(input("Coloque o primeiro número: "))
+        while num2 == "":
+            num2 = int(input("Coloque o segundo número: "))
+        if opcao == 1: #Adição
+            print(num1+num2)
+            
+        elif opcao == 2: #Subtração
+            print(num1-num2)
+        
+        elif opcao == 3: #Multiplicação
+            print(num1*num2)
+            
+        elif opcao == 4: #Divisão
+            print(num1/num2)
+            
+        elif opcao == 5:#Sair
+            print("Saindo do sistema...")
+            break
+    else:
+        print("Opção Inválida!")
